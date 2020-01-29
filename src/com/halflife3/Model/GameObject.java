2e41f35fffbcf01f51048a3cb7899cea04361a38
Johnathon Kemp <kemp133@googlemail.com>
2020-01-29 18:15:47 +0000

    Started implementing the GameObject class, adding a reference to Vector2 for the position of the object and also a short to express the rotation of the object. Also added a square distance function to Vector2, as this doesn't make a costly call to square root and can therefore be used as a optimisation step in some cases

--- src/com/halflife3/Model/GameObject.java
+++ src/com/halflife3/Model/GameObject.java
@@ -1,4 +1,8 @@
 package com.halflife3.Model;
 
-public class GameObject {
+public abstract class GameObject {
+    //Using a Vector2 to represent the location of a game object in the world
+    private Vector2 position;
+    //Using a short as a large number isn't needed to represent the rotation of the object
+    private short rotation = 0;
 }
