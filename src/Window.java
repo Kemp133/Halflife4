a3f9d2af9403a668544d27ab54a3f7f57d2a6c97
Joe <joefeehily@live.co.uk>
2020-02-05 12:50:26 +0000

    Start of new Window build

--- src/Window.java
+++ src/Window.java
@@ -1,43 +1,61 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import java.util.Collection;
 
-public class Window {
+public class Window extends Application {
 
-    private JFrame frame;
-    private BufferedImage image;
-    private Canvas canvas;
-    private BufferStrategy bufferStrategy;
-    private Graphics graphics;
+    Stage window;
+    BorderPane layout;
 
-    public Window (GameContainer gc) {
-        image = new BufferedImage(gc.getWidth(), gc.getHeight(), BufferedImage.TYPE_INT_RGB);
-        canvas = new Canvas();
-        Dimension s = new Dimension((int)(gc.getWidth() * gc.getScale()), (int)(gc.getHeight() * gc.getScale()));
-        canvas.setPreferredSize(s);
-        canvas.setMinimumSize(s);
-        canvas.setMaximumSize(s);
-
-        frame = new JFrame(gc.getTitle());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.add(canvas, BorderLayout.CENTER);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setVisible(true);
-
-        canvas.createBufferStrategy(2);
-        bufferStrategy = canvas.getBufferStrategy();
-        graphics = bufferStrategy.getDrawGraphics();
+    private void addButtons() {
+        Button newGButton = new Button("New Game");
+        Button optButton = new Button("Options");
+        Button quit = new Button("Quit");
+        newGButton.setLayoutX(250);
+        newGButton.setLayoutY(220);
+        StackPane root = new StackPane();
+        root.getChildren().add(newGButton);
     }
 
-    public void Update() {
-        graphics.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(), null);
-        bufferStrategy.show();
+    public static void main (String[] args) {
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       // Scene scene = new Scene(createContent());
+        primaryStage.setTitle("Team HalfLife");
+        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.show();
+    }
 
+    /*public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Team HalfLife");
+        newGButton = new Button("New Game");
+        optButton = new Button("Options");
+        quit = new Button("Quit");
 
+        StackPane stack = new StackPane();
+        stack.getChildren().addAll(newGButton);
+
+        //Group root = new Group();
+        Scene s = new Scene(stack, 300, 300);
+
+        primaryStage.setScene(s);
+        primaryStage.show();
+        /*Rectangle r = new Rectangle(25,25,250,250);
+        r.setFill(Color.BLUE);
+
+        root.getChildren().add(r);*/
+
+        /*StackPane stack = new StackPane();
+        stack.getChildren().addAll(new Rectangle(100,100,Color.BLUE), newGButton, optButton, quit);
+    }*/
 }
