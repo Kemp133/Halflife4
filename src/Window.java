7bd08286f797ed51093d442a7ddab0841b486697
Joe <joefeehily@live.co.uk>
2020-02-05 15:42:06 +0000

    Creating a button still seems to break it at this point. But it adds a background image for the window

--- src/Window.java
+++ src/Window.java
@@ -15,14 +15,13 @@ import java.io.FileNotFoundException;
 
 public class Window extends Application {
 
-    Stage window;
-    BorderPane layout;
+    /*Stage window;
+    BorderPane layout;*/ // Not needed for now
 
     private StackPane pane = new StackPane();
 
     private static final double SCREEN_WIDTH = Screen.getPrimary().getBounds().getWidth();
     private static final double SCREEN_HEIGHT = Screen.getPrimary().getBounds().getHeight();
-
 
 
     private void addButtons() throws FileNotFoundException {
@@ -48,12 +47,6 @@ public class Window extends Application {
         pane.getChildren().add(button);
     }
     private void addBackground() throws FileNotFoundException {
-        /*ImageView imageView = new ImageView(new Image(getClass().getResource("res/markus-henze-4vVZ8N88Ygs-unsplash.jpg").toExternalForm()));
-        imageView.setFitWidth(1280);
-        imageView.setFitHeight(720);
-
-        pane.getChildren().add(imageView);*/
-
         FileInputStream inputStream = new FileInputStream("res/markus-henze-4vVZ8N88Ygs-unsplash.jpg");
         Image image = new Image(inputStream);
 
@@ -75,10 +68,7 @@ public class Window extends Application {
     public void start(Stage primaryStage) {
         try {
             primaryStage.setTitle("Team HalfLife");
-            addBackground();
-            addButtons();
-            Scene scene = new Scene(pane, SCREEN_WIDTH, SCREEN_HEIGHT, Color.WHITE);
-            //primaryStage.setScene(new Scene(pane, 500, 500, Color.BLACK));
+            Scene scene = new Scene(createContent(), SCREEN_WIDTH, SCREEN_HEIGHT, Color.WHITE);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
@@ -89,27 +79,4 @@ public class Window extends Application {
     public static void main (String[] args) {
         launch(args);
     }
-
-    /*public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Team HalfLife");
-        newGButton = new Button("New Game");
-        optButton = new Button("Options");
-        quit = new Button("Quit");
-
-        StackPane stack = new StackPane();
-        stack.getChildren().addAll(newGButton);
-
-        //Group root = new Group();
-        Scene s = new Scene(stack, 300, 300);
-
-        primaryStage.setScene(s);
-        primaryStage.show();
-        /*Rectangle r = new Rectangle(25,25,250,250);
-        r.setFill(Color.BLUE);
-
-        root.getChildren().add(r);*/
-
-        /*StackPane stack = new StackPane();
-        stack.getChildren().addAll(new Rectangle(100,100,Color.BLUE), newGButton, optButton, quit);
-    }*/
 }
