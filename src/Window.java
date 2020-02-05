114d9910a00da4ccb5deb7b04320f410e3a2e4bf
Joe <joefeehily@live.co.uk>
2020-02-05 15:14:10 +0000

    Continued attempt at getting button to work

--- src/Window.java
+++ src/Window.java
@@ -1,39 +1,93 @@
 import javafx.application.Application;
-import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.scene.Node;
-import java.util.Collection;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Window extends Application {
 
     Stage window;
     BorderPane layout;
 
-    private void addButtons() {
-        Button newGButton = new Button("New Game");
-        Button optButton = new Button("Options");
-        Button quit = new Button("Quit");
-        newGButton.setLayoutX(250);
-        newGButton.setLayoutY(220);
-        StackPane root = new StackPane();
-        root.getChildren().add(newGButton);
+    private StackPane pane = new StackPane();
+
+    private static final double SCREEN_WIDTH = Screen.getPrimary().getBounds().getWidth();
+    private static final double SCREEN_HEIGHT = Screen.getPrimary().getBounds().getHeight();
+
+
+
+    private void addButtons() throws FileNotFoundException {
+        //Button newGButton = new Button("New Game");
+        //Button optButton = new Button("Options");
+        //Button quit = new Button("Quit");
+        //newGButton.setLayoutX(250);
+        //newGButton.setLayoutY(220);
+        //newGButton.setCenterShape(true);
+        //pane.getChildren().addAll(newGButton, optButton, quit);
+
+        //Adds an image to a button
+        FileInputStream input = new FileInputStream("res/button_image.png");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+
+        Button button = new Button("New Game", imageView);
+        //
+
+        //ImageView imageView = new ImageView(new Image(getClass().getResource("/res/button_image.png").toExternalForm()));
+        //Button testButton = new Button("", imageView);
+
+        pane.getChildren().add(button);
+    }
+    private void addBackground() throws FileNotFoundException {
+        /*ImageView imageView = new ImageView(new Image(getClass().getResource("res/markus-henze-4vVZ8N88Ygs-unsplash.jpg").toExternalForm()));
+        imageView.setFitWidth(1280);
+        imageView.setFitHeight(720);
+
+        pane.getChildren().add(imageView);*/
+
+        FileInputStream inputStream = new FileInputStream("res/markus-henze-4vVZ8N88Ygs-unsplash.jpg");
+        Image image = new Image(inputStream);
+
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(SCREEN_WIDTH);
+        imageView.setFitHeight(SCREEN_HEIGHT);
+
+        pane.getChildren().add(imageView);
+    }
+
+    private StackPane createContent() throws FileNotFoundException {
+        addBackground();
+        addButtons();
+
+        return pane;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            primaryStage.setTitle("Team HalfLife");
+            addBackground();
+            addButtons();
+            Scene scene = new Scene(pane, SCREEN_WIDTH, SCREEN_HEIGHT, Color.WHITE);
+            //primaryStage.setScene(new Scene(pane, 500, 500, Color.BLACK));
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 
     public static void main (String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-       // Scene scene = new Scene(createContent());
-        primaryStage.setTitle("Team HalfLife");
-        primaryStage.setScene(new Scene(root, 300, 300));
-        primaryStage.show();
     }
 
     /*public void start(Stage primaryStage) throws Exception {
