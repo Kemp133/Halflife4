8bd2c717ab13c85be6544057cf7e7bc21c28cdf5
Joe <joefeehily@live.co.uk>
2020-02-06 17:30:16 +0000

    Database is now working with a local server I've set up. It can print a string (name) and int (score) from a table, TopLeaderboard, in the database, Leaderboard.

--- src/Database.java
+++ src/Database.java
@@ -14,22 +14,32 @@ public class Database {
             Class.forName("org.postgresql.Driver");
             c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Leaderboard", "postgres", "123");
 
+            if (c != null) {
+                System.out.println("Connected successfully");
+            } else {
+                System.out.println("Connection failed");
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
 
         try {
-            PreparedStatement pst = c.prepareStatement("SELECT * FROM public.Top Leaderboard");
+            PreparedStatement pst = c.prepareStatement("SELECT * FROM \"TopLeaderboard\""); //or leaderboard?
+            if (pst != null) {
+                System.out.println("Success");
+            } else {
+                System.out.println("Failed");
+            }
             ResultSet rs = pst.executeQuery();
 
             while (rs.next()) {
 
-                System.out.print(rs.getInt(1));
+                System.out.print(rs.getString(1));
                 System.out.print(": ");
-                System.out.println(rs.getString(2));
+                System.out.println(rs.getInt(2));
             }
 
         } catch (SQLException ex) {
