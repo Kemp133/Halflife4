a81dcd9a6f4ea7e73ac278df33cbef0976119aec
Thomas Shun Hau <sxh1154@student.bham.ac.uk>
2020-02-10 11:10:03 +0000

    Merge branch 'master' of C:\Users\Thomas\IdeaProjects\halflife0 with conflicts.

--- src/com/halflife3/Model/Enemy.java
+++ src/com/halflife3/Model/Enemy.java
@@ -1,6 +1,20 @@
-package com.halflife3.Model;
-
-//this is to store the abstract data for all enemy types, we are expected to create specific enemies tha implements this
-abstract class Enemy extends Character{
-    // we need some basic AI here, move(), shoot(), melee()
-}
+//package com.halflife3.Model;
+//
+//import com.halflife3.Model.GameObject;
+////this is to store the abstract data for all enemy types, we are expected to create specific enemies tha implements this
+//abstract class Enem{
+//    // we need some basic AI here, move(), shoot(), melee()
+//   private int hp;
+//   private int movementSpeed;
+//   private int damage;
+//
+//   public Enemy(int x, int y){
+//
+//   }
+//   public void move(int speed){
+//
+//   }
+//   public void attack(int dama){
+//
+//   }
+//}
