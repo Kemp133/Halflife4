40b5b32f7ca4ce2d2f664fad1b968b29157c0eb0
Thomas Shun Hau <sxh1154@student.bham.ac.uk>
2020-02-14 14:47:53 +0000

    updated explodeEnemy

--- src/com/halflife3/Model/explodeEnemy.java
+++ src/com/halflife3/Model/explodeEnemy.java
@@ -4,10 +4,10 @@ import com.halflife3.Controller.ObjectManager;
 
 public class explodeEnemy extends Enemy{
 
-    public explodeEnemy(Vector2 position, Vector2 velocity, short rotation, ObjectManager om, int life) {
-        super(position, velocity, rotation, om, life);
+    public explodeEnemy(Vector2 position, Vector2 velocity, short rotation, ObjectManager om) {
+        super(position, velocity, rotation, om, 2);
     }
-
+    //TODO: write the method
     @Override
     public void attackPattern() {
         //find closest player
