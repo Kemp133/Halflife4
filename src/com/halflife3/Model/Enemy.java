d25bad45ee9a54e8dd9ee21f5a208531342288d6
sxl1046 <sxl1046@bham.ac.uk>
2020-02-17 19:03:08 +0000

    useless Update ignore plz

--- src/com/halflife3/Model/Enemy.java
+++ src/com/halflife3/Model/Enemy.java
@@ -1,7 +1,25 @@
 /*
 package com.halflife3.Model;
 
+import com.halflife3.Model.GameObject;
 //this is to store the abstract data for all enemy types, we are expected to create specific enemies tha implements this
-abstract class Enemy extends Character{
+abstract class Enem{
     // we need some basic AI here, move(), shoot(), melee()
+<<<<<<< .merge_file_a46872
 }*/
+=======
+   private int hp;
+   private int movementSpeed;
+   private int damage;
+
+   public Enemy(int x, int y){
+
+   }
+   public void move(int speed){
+
+   }
+   public void attack(int dama){
+
+   }
+}
+>>>>>>> .merge_file_a38376
--- src/com/halflife3/Model/ExplodingEnemy.java
+++ src/com/halflife3/Model/ExplodingEnemy.java
@@ -1,4 +1,10 @@
+<<<<<<< .merge_file_a46676
 /*
 public class ExplodingEnemy extends Enemy{
 
 }*/
+=======
+public class ExplodingEnemy extends Enemy{
+
+}
+>>>>>>> .merge_file_a40576
