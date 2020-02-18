29ec684f6c4da91f5a01c4949bcee2b6e174e6be
Joe <joefeehily@live.co.uk>
2020-02-18 17:34:43 +0000

    User (Class added) -- This has been added to populate the observablelist for the tableview in Leaderboard
    Leaderboard -- This now works with the remote database - This will print the users from highest to lowest score in a table showing the username and score

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -9,50 +9,34 @@ import javafx.stage.Stage;
 import java.sql.*;
 
 public class Leaderboard extends Application {
-    //Stage window;
-    //TableView<Product> table;
-
     private ObservableList<ObservableList> data;
     private TableView tableview;
 
-    public void getData() throws SQLException {
+    public ResultSet getData() throws SQLException {
         Connection c = null;
+        PreparedStatement preparedStatement = null;
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Leaderboard", "postgres", "123");
+            String url = "jdbc:postgresql://rogue.db.elephantsql.com:5432/nuzmlzpr";
+            url = url.trim();
+            c = DriverManager.getConnection(url, "nuzmlzpr", "pd7OdC_3BiVrAPNU68CETtFtBaqFxJFB");
 
-            if (c != null) {
-                System.out.println("Connected successfully");
-            } else {
-                System.out.println("Connection failed");
-            }
+            //Creating the query
+            String query = "Select * from userdatascore order by score desc";
+            //Creating the Statement
+            preparedStatement = c.prepareStatement(query);
+            //Executing the query
+            return preparedStatement.executeQuery();
 
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
+        } finally {
+            //closeConnections(c, preparedStatement, null); //TODO: Fix - Not working currently - Seems to remove the result set somehow as well, maybe something to do with closing the prepared statement(more than likely)
+            c.close();
         }
-
-        //Creating the Statement
-        Statement stmt = c.createStatement();
-        String query = "Select * from \"TopLeaderboard\" where playername = 'Joe'";
-        //Executing the query
-        ResultSet rs = stmt.executeQuery(query);
-        //Retrieving ResultSetMetaData object
-        ResultSetMetaData rsMetaData = rs.getMetaData();
-        System.out.println("Column name: " + rsMetaData.getColumnName(1));
-        System.out.println("Column name: " + rsMetaData.getColumnName(2));
-        if (!rs.next()) { //No need to do rs.next() == false as rs.next returns a boolean, so to check for false you can just instead have !rs.next()
-            System.out.println("no match");
-        } else {
-            System.out.println("match");
-        }
-        while (rs.next()) {
-
-            System.out.print(rs.getString(1));
-            System.out.print(": ");
-            System.out.println(rs.getInt(2));
-        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -62,11 +46,43 @@ public class Leaderboard extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         tableview = new TableView();
-        getData();
+        ResultSet rs = getData();
+
+        TableColumn nameColumn = new TableColumn("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn scoreColumn = new TableColumn("Score");
+        scoreColumn.setCellValueFactory(new PropertyValueFactory<>("Score"));
+
+        tableview.getColumns().addAll(nameColumn, scoreColumn);
+
+        while (rs.next()) {
+            User user = new User(rs.getString(2), rs.getInt(3));
+            tableview.getItems().add(user);
+        }
 
         Scene scene = new Scene(tableview);
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    //TODO: May not be needed before prototype if issue not fixed
+    public void closeConnections(Connection c, PreparedStatement p, ResultSet r) {
+        if (c != null) {
+            try {
+                c.close();
+            } catch (SQLException e) { /* ignored */}
+        }
+        if (p != null) {
+            try {
+                p.close();
+            } catch (SQLException e) { /* ignored */}
+        }
+        if (r != null) {
+            try {
+                r.close();
+            } catch (SQLException e) { /* ignored */}
+        }
     }
 }
---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String name;
+    private int score;
+
+    public User(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
