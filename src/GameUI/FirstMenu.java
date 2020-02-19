141ad5198c4b00ce4438194e4104212a0b6abad8
sxl1046 <sxl1046@bham.ac.uk>
2020-02-19 11:04:35 +0000

    Updating

--- src/GameUI/FirstMenu.java
+++ src/GameUI/FirstMenu.java
@@ -1,23 +1,18 @@
 package GameUI;
 
 import com.halflife3.DatabaseUI.Login;
-import com.halflife3.Networking.Server.MainServer;
 import com.halflife3.Networking.Server.Server;
-import com.halflife3.View.Main;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
 
-import java.awt.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
