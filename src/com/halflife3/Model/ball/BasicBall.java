b624e69fe3c534471d27daff2c0f34ebadc91fdf
Thomas Shun Hau <sxh1154@student.bham.ac.uk>
2020-02-28 10:14:06 +0000

    added BasicBall class

---
+++ src/com/halflife3/Model/ball/BasicBall.java
@@ -0,0 +1,34 @@
+package com.halflife3.Model.ball;
+
+
+import com.halflife3.Controller.ObjectManager;
+import com.halflife3.Model.GameObject;
+import com.halflife3.Model.Vector2;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Shape;
+
+public class BasicBall extends GameObject{
+    public BasicBall(Vector2 position, Vector2 velocity, double rotation, ObjectManager om) {
+        super(position, velocity, rotation, om);
+    }
+
+    @Override
+    public Shape GetBounds() {
+        return null;
+    }
+
+    @Override
+    public boolean intersects(GameObject s) {
+        return false;
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+
+    }
+
+    @Override
+    public void update(double time) {
+
+    }
+}
