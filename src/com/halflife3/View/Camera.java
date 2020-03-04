c86b301b6db8e8025e440f5f6143939031cdeb94
Johnathon Kemp <kemp133@googlemail.com>
2020-03-04 13:41:57 +0000

    Made Camera static and made this be responsible for the offset of all GameObjects in the scene. Also made MapRender static and added some general improvements - Made MapRender SOLELY responsible for rendering and dealing with the map, as when a Block is created it is removed from the ObjectManager. Added a minimum width and height to the maps which we try and load to make sure they at least fit into the 800x600 window. Also added the file loading to the loadLevel instead, which means no reference for an image is required

--- src/com/halflife3/View/Camera.java
+++ src/com/halflife3/View/Camera.java
@@ -1,4 +1,25 @@
 package com.halflife3.View;
 
+import com.halflife3.Model.Vector2;
+
 public class Camera {
+	private static Vector2 offset;
+
+	public Camera() {}
+
+	static {
+		offset = new Vector2();
+	}
+
+	public static void SetOffset(Vector2 toSet) {
+		offset = toSet;
+	}
+	public static void SetOffsetX(double x) { offset.setX(x);}
+	public static void SetOffsetY(double y) { offset.setY(y);}
+
+	public static Vector2 GetOffset() {
+		return new Vector2(offset);
+	}
+	public static double GetOffsetX() { return offset.getX(); }
+	public static double GetOffsetY() { return offset.getY(); }
 }
