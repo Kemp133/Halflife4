3eab7f13e40f7ad1f43546bd21fd23eeb6391e35
Johnathon Kemp <kemp133@googlemail.com>
2020-03-05 14:29:28 +0000

    Merge branch 'master' of C:\Users\Lenovo\IdeaProjects\halflife with conflicts.

---
+++ src/com/halflife3/Model/Ball/BasicBall.java
@@ -0,0 +1,23 @@
+package com.halflife3.Model.Ball;
+
+import com.halflife3.Model.Sprite;
+import com.halflife3.Model.Vector2;
+import javafx.scene.canvas.GraphicsContext;
+
+public class BasicBall extends Sprite {
+    public BasicBall(Vector2 position, Vector2 velocity, double rotation) {
+        super(position, velocity, rotation);
+        setSprite("res/Sprites/Ball.png");
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        gc.drawImage(sprite, getPosX() - (getWidth() / 2), getPosY() - (getHeight() / 2));
+    }
+
+    @Override
+    public void update(double time) {
+
+    }
+
+}
