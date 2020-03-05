c42a01f41130a27e00f1cb42d46d21c51f368af9
Thomas Shun Hau <sxh1154@student.bham.ac.uk>
2020-03-05 13:32:11 +0000

    removed intersect from BasicBall.java

--- src/com/halflife3/Model/ball/BasicBall.java
+++ src/com/halflife3/Model/ball/BasicBall.java
@@ -1,6 +1,5 @@
 package com.halflife3.Model.ball;
 
-import com.halflife3.Model.GameObject;
 import com.halflife3.Model.Sprite;
 import com.halflife3.Model.Vector2;
 import javafx.scene.canvas.GraphicsContext;
@@ -9,11 +8,6 @@ public class BasicBall extends Sprite {
     public BasicBall(Vector2 position, Vector2 velocity) {
         super(position, velocity);
         setSprite("res/balls/basicBall.png");
-    }
-
-    @Override
-    public boolean intersects(GameObject s) {
-        return false;
     }
 
     @Override
