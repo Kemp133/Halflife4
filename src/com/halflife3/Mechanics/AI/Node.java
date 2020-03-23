000ee0e894415513a27a8dc4e0f6c2e5fe935cab
Dominykas Gudauskas <dxg857@cs.bham.ac.uk>
2020-03-23 16:10:14 +0000

    Wrote the first draft for the AI algorithm, definitely too slow right now, will need to optimise it

---
+++ src/com/halflife3/Mechanics/AI/Node.java
@@ -0,0 +1,23 @@
+package com.halflife3.Mechanics.AI;
+
+import com.halflife3.Mechanics.Vector2;
+
+import java.util.ArrayList;
+
+public class Node {
+    public String type = "Free"; // "Free" || "Wall"
+    public double h = 0; // NodeToEnd
+    public double g = 0; // StartToNode
+    public double f = 0; // totalCost
+    public ArrayList<Node> children = new ArrayList<>();
+    private Node parent;
+    public Vector2 position; // Position on the map
+
+    public Node (Vector2 position) { this.position = position; }
+
+    public void addChild(Node child) { children.add(child); }
+
+    public Node getParent() { return parent; }
+
+    public void setParent(Node parent) { this.parent = parent; }
+}
