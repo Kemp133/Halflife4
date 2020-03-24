ad0595d62392923e31851ec433cbb4e1edf6a7c5
Dominykas Gudauskas <dxg857@cs.bham.ac.uk>
2020-03-24 19:06:16 +0000

    Tidied up BasicBall.java

--- src/com/halflife3/Mechanics/GameObjects/BasicBall.java
+++ src/com/halflife3/Mechanics/GameObjects/BasicBall.java
@@ -5,7 +5,6 @@ import com.halflife3.View.Camera;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.shape.Circle;
 
-
 public class BasicBall extends Sprite {
     public Circle circle;
     Vector2 original_position;
@@ -19,7 +18,6 @@ public class BasicBall extends Sprite {
                 Math.max(getWidth(), getHeight()) / 2 + 1);
     }
 
-
     @Override
     public void render(GraphicsContext gc) {
         gc.drawImage(sprite, getPosX() - Camera.GetOffsetX(), getPosY() - Camera.GetOffsetY());
@@ -28,38 +26,41 @@ public class BasicBall extends Sprite {
     @Override
     public void update(double time) {
         original_position = new Vector2(position);
-        if (velocity.getX() * (velocity.getX() - acc.getX()) > 0)
-            velocity.setX(velocity.getX() - acc.getX());
-        else
-            velocity.setX(0);
-        if (velocity.getY()*(velocity.getY() - acc.getY()) > 0)
-            velocity.setY(velocity.getY() - acc.getY());
-        else
-            velocity.setY(0);
-        position = position.add(new Vector2(velocity).multiply(time));
-        circle.setCenterX(position.getX() + 12);
-        circle.setCenterY(position.getY() + 12);
+        double velX = velocity.getX() - acc.getX();
+        double velY = velocity.getY() - acc.getY();
+
+        velocity.setX((velocity.getX() * velX > 0) ? velX : 0);
+        velocity.setY((velocity.getY() * velY > 0) ? velY : 0);
+
+        position.add(new Vector2(velocity).multiply(time));
+        circle.setCenterX(position.getX() + getWidth() / 2);
+        circle.setCenterY(position.getY() + getHeight() / 2);
     }
 
     public void collision(int bounce, double time) {
 
-        if (bounce == 1) {
-            this.position = original_position;
-            velocity.setX(-velocity.getX());
-            acc.setX(-acc.getX());
+        switch (bounce) {
+            case 1 : {
+                position = original_position;
+                velocity.setX(-velocity.getX());
+                acc.setX(-acc.getX());
+            }
+
+            case 2 : {
+                position = original_position;
+                velocity.setY(-velocity.getY());
+                acc.setY(-acc.getY());
+            }
+
+            case 3 : {
+                position = original_position;
+                velocity = new Vector2(0,0);
+                acc= new Vector2(0,0);
+            }
         }
-        if(bounce == 2){
-            this.position = original_position;
-            velocity.setY(-velocity.getY());
-            acc.setY(-acc.getY());
-        }
-        if(bounce == 3){
-            this.position = original_position;
-            velocity = new Vector2(0,0);
-            acc= new Vector2(0,0);
-        }
-        circle.setCenterX(position.getX() + 12);
-        circle.setCenterY(position.getY() + 12);
+
+        circle.setCenterX(position.getX() + getWidth() / 2);
+        circle.setCenterY(position.getY() + getHeight() / 2);
     }
 
 }
