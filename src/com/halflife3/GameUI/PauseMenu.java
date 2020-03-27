84788b9aef224d6a1c7475600e5d3f6227fb9035
Thomas Shun Hau <sxh1154@student.bham.ac.uk>
2020-03-27 14:23:37 +0000

    added button names to PauseMenu

--- src/com/halflife3/GameUI/PauseMenu.java
+++ src/com/halflife3/GameUI/PauseMenu.java
@@ -1,4 +1,10 @@
 package com.halflife3.GameUI;
 
 public class PauseMenu {
+    public boolean isPause = false;
+    public String menuName = "GAME MENU";
+    public String backToGame = "Back to Game";
+    public String options = "Options";
+    public String quitToMenu = "Quit to main menu";
+    public String quitToDesktop = "Quit to desktop";
 }
