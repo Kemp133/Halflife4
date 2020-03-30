002cb97c9e2ff73699773915fd8ac76420cc4c1b
Thomas Shun Hau <sxh1154@student.bham.ac.uk>
2020-03-30 21:57:21 +0100

    got the PauseMenu working

--- src/com/halflife3/GameUI/PauseMenu.java
+++ src/com/halflife3/GameUI/PauseMenu.java
@@ -1,24 +1,25 @@
 package com.halflife3.GameUI;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.GaussianBlur;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.stage.StageStyle;
+import javafx.scene.input.KeyCode;
 
 public class PauseMenu extends Application {
+    private boolean pause = false;
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -26,48 +27,50 @@ public class PauseMenu extends Application {
         Button pauseButton = new Button("Pause");
         Pane pane = new Pane();
         pane.setMinSize(600, 400);
-        BorderPane root = new BorderPane(pane, null, null, pauseButton, new Label("This is\nthe test scene"));
+        BorderPane root = new BorderPane(pane, null, null, pauseButton, new Label(""+pause));
+        //if esc is pressed
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(keyEvent.getCode()==KeyCode.ESCAPE){
+                    pause = !pause;
+                    //set background effect
+                    root.setEffect(new GaussianBlur());
+                    //setting up vbox for buttons
+                    //we will have 1 line of description with 4 buttons, that 5 rows
+                    VBox pauseRoot = new VBox(5);
+                    pauseRoot.setStyle("-fx-background-color: rgba(255, 255, 255, 0.8);");
+                    pauseRoot.setAlignment(Pos.CENTER);
+                    pauseRoot.setPadding(new Insets(20));
+                    //actual stuff in the rows
+                    pauseRoot.getChildren().add(new Label("Paused"));
+                    //Button 1
+                    Button returnToGame = new Button("Return to game");
+                    pauseRoot.getChildren().add(returnToGame);
+                    //Button 2
+                    Button options = new Button("Options");
+                    pauseRoot.getChildren().add(options);
+                    //Button 3
+                    Button toMainMenu = new Button("Quit to Main Menu");
+                    pauseRoot.getChildren().add(toMainMenu);
+                    //Button 4
+                    Button toDesktop = new Button("Quit to Desktop");
+                    pauseRoot.getChildren().add(toDesktop);
 
-        //when button is pressed
-        pauseButton.setOnAction(e -> {
-            //set background effect
-            root.setEffect(new GaussianBlur());
-            //setting up vbox for buttons
-            //we will have 1 line of description with 4 buttons, that 5 rows
-            VBox pauseRoot = new VBox(5);
-            pauseRoot.setStyle("-fx-background-color: rgba(255, 255, 255, 0.8);");
-            pauseRoot.setAlignment(Pos.CENTER);
-            pauseRoot.setPadding(new Insets(20));
-            //actual stuff in the rows
-            pauseRoot.getChildren().add(new Label("Paused"));
-            //Button 1
-            Button returnToGame = new Button("Return to game");
-            pauseRoot.getChildren().add(returnToGame);
-            //Button 2
-            Button options = new Button("Options");
-            pauseRoot.getChildren().add(options);
-            //Button 3
-            Button toMainMenu = new Button("Quit to Main Menu");
-            pauseRoot.getChildren().add(toMainMenu);
-            //Button 4
-            Button toDesktop = new Button("Quit to Desktop");
-            pauseRoot.getChildren().add(toDesktop);
-
-            //actual stage of the pause menu
-            Stage popupStage = new Stage(StageStyle.TRANSPARENT);
-            popupStage.initOwner(stage);
-            popupStage.initModality(Modality.APPLICATION_MODAL);
-            popupStage.setScene(new Scene(pauseRoot, Color.TRANSPARENT));
-            //Button function 1:resume
-            returnToGame.setOnAction(event-> {
-                root.setEffect(null);
-                popupStage.hide();
+                    //actual stage of the pause menu
+                    Stage popupStage = new Stage(StageStyle.TRANSPARENT);
+                    popupStage.initOwner(stage);
+                    popupStage.initModality(Modality.APPLICATION_MODAL);
+                    popupStage.setScene(new Scene(pauseRoot, Color.TRANSPARENT));
+                    //Button function 1:resume
+                    returnToGame.setOnAction(event-> {
+                        root.setEffect(null);
+                        popupStage.hide();
                     });
-            popupStage.show();
-                });
-        //stats of the pause button
-        BorderPane.setAlignment(pauseButton, Pos.CENTER);
-        BorderPane.setMargin(pauseButton, new Insets(5));
+                    popupStage.show();
+                }
+            }
+        });
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -77,22 +80,4 @@ public class PauseMenu extends Application {
 
         Application.launch(args);
     }
-    /*
-    //buttons that need to be in pause
-    public VBox pauseVBox(){
-        //Create buttons:
-        Button returnToGame = new Button("Return to game");
-        Button options = new Button("Options");
-        Button toMainMenu = new Button("Quit to Main Menu");
-        Button toDesktop = new Button("Quit to Desktop");
-        //edit button properties:
-        returnToGame.setStyle("-fx-font-size: 2em; ");
-        options.setStyle("-fx-font-size: 2em; ");
-        toMainMenu.setStyle("-fx-font-size: 2em; ");
-        toDesktop.setStyle("-fx-font-size: 2em; ");
-        return new VBox(returnToGame,options,toMainMenu,toDesktop);
-    }
-    public void pauseActions(BorderPane bdpane, VBox vbox){
-
-    }*/
 }
