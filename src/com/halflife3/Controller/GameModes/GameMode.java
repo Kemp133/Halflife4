e9ac27f55f3c8c1713f1b122ea8b8f8572e60f23
Johnathon Kemp <jrk846@student.bham.ac.uk>
2020-03-31 18:09:09 +0100

    Tidied up the GameMode class

--- src/com/halflife3/Controller/GameModes/GameMode.java
+++ src/com/halflife3/Controller/GameModes/GameMode.java
@@ -2,25 +2,21 @@ package com.halflife3.Controller.GameModes;
 
 public abstract class GameMode {
 	protected String GameModeName;
-//	protected boolean CanRespawn;
 
-	public GameMode(String GameModeName) {
-		this.GameModeName = GameModeName;
-	}
-
+	public GameMode(String GameModeName) { this.GameModeName = GameModeName; }
 	abstract void initialise();
 	abstract void gameLoop();
 	abstract void finished();
 	abstract boolean won(int team);
 	abstract boolean lost();
+	public void runGame() {
+		initialise();
+		gameLoop();
+		finished();
+	}
 
 	//region Get/Set GameModeName
 	public String getGameModeName() { return GameModeName; }
 	public void setGameModeName(String gameModeName) { GameModeName = gameModeName; }
 	//endregion
-
-//	//region Get/Set CanRespawn
-//	public boolean getCanRespawn () { return CanRespawn; }
-//	public void setCanRespawn (boolean canRespawn) { CanRespawn = canRespawn; }
-//	//endregion/
 }
