fe75301a4243febf9b5d39285101f88c95544aeb
Shuhao Zhang <33047941+Shuhao99@users.noreply.github.com>
2020-04-11 01:46:54 +0800

    Modify the lose scene.

--- src/com/halflife3/GameUI/LoseScene.java
+++ src/com/halflife3/GameUI/LoseScene.java
@@ -1,22 +1,13 @@
 package com.halflife3.GameUI;
 
-import com.halflife3.Controller.ClientController;
-import com.halflife3.Controller.Exceptions.SceneDoesNotExistException;
-import com.halflife3.Controller.Exceptions.SceneStackEmptyException;
-import com.halflife3.Controller.MapMenuController;
+import com.halflife3.Controller.MenuController;
 import com.halflife3.Controller.SceneManager;
-import com.halflife3.DatabaseUI.Leaderboard;
-import com.halflife3.DatabaseUI.SettingsMenu;
-import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.layout.*;
-import javafx.scene.text.Font;
+import javafx.scene.text.*;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -26,12 +17,11 @@ import java.io.FileNotFoundException;
  * This class acts as a view for the Final stage of the game, if you win the game you will see this page.
  */
 public class LoseScene /*extends Application*/ {
-    private BorderPane borderPane;
-    private VBox vbox;
+    private              VBox   vbox;
     private static final String MENU_BACKGROUND_IMAGE_LOCATION = "res/Leaderboard/LeaderboardBackground.jpg";
-    private static final String Win_pic = "res/numbers/lose.png";
-    private Button exit = new Button("Back");
-    private Scene scene;
+    private static final String Win_pic                        = "res/numbers/lose.png";
+    private              Button exit                           = new Button("Back");
+    private              Scene  scene;
 
     public LoseScene() {
         initialiseMenuScene();
@@ -42,23 +32,27 @@ public class LoseScene /*extends Application*/ {
             MainMenu main = new MainMenu();
             SceneManager.getInstance().setScene("Main Menu", main.getScene());
         });
+
         VBox vbox2 = new VBox(exit);
         vbox2.setAlignment(Pos.CENTER);
-        vbox2.setPadding(new Insets(50,0,0,0));
-        try{
-            Image win_image = new Image(new FileInputStream(Win_pic));
-            ImageView Win_show = new ImageView(win_image);
-            vbox = new VBox(Win_show,vbox2);
-        }catch (FileNotFoundException e){
+        vbox2.setPadding(new Insets(50, 0, 0, 0));
+
+        try {
+            Image     win_image = new Image(new FileInputStream(Win_pic));
+            ImageView Win_show  = new ImageView(win_image);
+            vbox = new VBox(Win_show, vbox2);
+        } catch (FileNotFoundException e) {
             System.out.println("Image not found");
         }
+
         vbox.setAlignment(Pos.BASELINE_CENTER);
         vbox.setPadding(new Insets(120, 0, 40, 0));
+
         return vbox;
     }
 
     private BorderPane borderPane() {
-        borderPane = new BorderPane();
+        BorderPane borderPane = new BorderPane();
 
         Font paladinFont = null;
         try {
@@ -71,18 +65,18 @@ public class LoseScene /*extends Application*/ {
         title.setFont(paladinFont);
         title.setStyle("-fx-text-fill: linear-gradient(#0000FF, #FFFFFF 95%);");
 
-        Insets insets = new Insets(30);
-        Pane topPane = new Pane();
+        Insets insets  = new Insets(30);
+        Pane   topPane = new Pane();
         topPane.setMinSize(500, 100);
         Pane leftPane = new Pane();
-        leftPane.setMinSize(100, 400);
+        leftPane.setMinSize(0, 0);
         Pane rightPane = new Pane();
-        rightPane.setMinSize(100, 400);
+        rightPane.setMinSize(0, 0);
         Pane bottomPane = new Pane();
         bottomPane.setMinSize(500, 100);
         borderPane.setTop(title);
         BorderPane.setMargin(title, insets);
-        borderPane.setAlignment(title, Pos.TOP_CENTER);
+        BorderPane.setAlignment(title, Pos.TOP_CENTER);
 
         borderPane.setLeft(leftPane);
         borderPane.setRight(rightPane);
@@ -98,7 +92,7 @@ public class LoseScene /*extends Application*/ {
     }
 
     private void initialiseMenuScene() {
-        scene = new Scene(borderPane(), 800, 600);
+        scene = new Scene(borderPane(), MenuController.GAME_WINDOW_WIDTH, MenuController.GAME_WINDOW_HEIGHT);
     }
 
     public Scene getScene() {
